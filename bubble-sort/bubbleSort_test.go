e9d06113c2292527991097c281aff8d896aae06b
Ase Deliri <deliri.ase@gmail.com>
2017-03-21 12:34:16 -0400

    added table tests for bubblesort

---
+++ bubble-sort/bubbleSort_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	cases := []struct {
+		in   []int
+		want []int
+	}{
+		{[]int{5, 4, 3, 2, 1},
+			[]int{1, 2, 3, 4, 5}},
+
+		{[]int{6039, 5039, 4837, 3382, 100},
+			[]int{100, 3382, 4837, 5039, 6039}},
+	}
+	for _, c := range cases {
+		got := BubbleSort(c.in)
+		if !reflect.DeepEqual(got, c.want) {
+			t.Errorf("BubbleSort(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
